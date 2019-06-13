914a2519c197598c01a53ac7a7823ed97cd1110a
Jamie Pask <jamiepask1392@gmail.com>
2019-06-13 16:26:02 -0700

    Entry page information removed

--- src/entry/landing.jsx
+++ src/entry/landing.jsx
@@ -6,39 +6,18 @@ import ReactSVG from 'react-svg';
 export default class Landing extends Component {
 
 	render() {
-		var words = ["Full stack ", "developer, ", "problem ", "solver and ", "digital ", "design ", "enthusist"];
-
-
+	
 		return (
 			<div className="container-max" id="landing-container">
 
 				<div id='landing-inner-container'>
-					<ReactSVG 	src="../../assets/circle-icon.svg" 
-									svgClassName="circle-icon-svg"
-									 />
 					<div className="col rhs" >
 						<div id='header-text-container'>
-						{ 
-							words.map(function(word) {
-								return (
-									<span className="header-mask" key={word}>
-									<CSSTransition 	in={true} 
-													timeout={0}
-													appear={true}
-													classNames="header-words">
-										<h1>{word}</h1>
-									</CSSTransition>
-									</span>
-								)
-							}) 
-						}
+							ADD SVG
 						</div>
 					</div>
 					<div className="col lhs" id='intro-text-container'>
 						<b>Living in LA</b>
-
-						<p>Hey! I’m Jamie, a full stack developer and a passionate tinkerer of all things software. Still learning and still growing but please take a look around. Any feedback is welcome! 
-						</p>
 						<hr></hr>
 					</div>
 				</div>
